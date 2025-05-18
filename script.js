6dbaa87bc7884797b0463cf357d1e4894054fc86
abdulrauf242 <ja3407519@gmail.com>
2025-05-18 06:14:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  let quantity = 1;
+  const quantityElement = document.getElementById("quantity");
+
+  function increaseQty() {
+    quantity++;
+    quantityElement.textContent = quantity;
+  }
+
+  function decreaseQty() {
+    if (quantity > 1) {
+      quantity--;
+      quantityElement.textContent = quantity;
+    }
+  }
+
